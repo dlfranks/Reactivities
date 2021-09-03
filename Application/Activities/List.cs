e815a997629b7bb259e635f1070a45ab400cf834
Deana Franks <deana.franks@woodplc.com>
2021-09-03 11:56:03 -0400

    end of section 4

--- Application/Activities/List.cs
+++ Application/Activities/List.cs
@@ -12,34 +12,24 @@ namespace Application.Activities
 {
     public class List
     {
-        public class Query : IRequest<List<Activity>>{
-            public CancellationToken ct;
+        public class Query : IRequest<List<Activity>>
+        {
+
         }
 
         public class Handler : IRequestHandler<Query, List<Activity>>
         {
             private readonly DataContext _context;
-            private readonly ILogger<List> _logger;
-            public Handler(DataContext context, ILogger<List> logger)
+            public Handler(DataContext context)
             {
                 _context = context;
-                _logger = logger;
+
+
             }
             public async Task<List<Activity>> Handle(Query request, CancellationToken cancellationToken)
             {
-                try{
-                    for(var i = 0; i < 10; i++)
-                    {
-                        cancellationToken.ThrowIfCancellationRequested();
-                        await Task.Delay(1000, cancellationToken);
-                        _logger.LogInformation($"Task {i} has completed.");
-                    }
-                }catch(Exception ex) when (ex is TaskCanceledException)
-                {
-                    
-                    _logger.LogInformation("Task was cancelled.");
-                }
-                return await _context.Activities.ToListAsync(request.ct);
+
+                return await _context.Activities.ToListAsync(cancellationToken);
             }
         }
     }
